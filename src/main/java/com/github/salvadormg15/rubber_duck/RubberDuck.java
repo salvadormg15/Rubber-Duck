96515c67d9e16bf8a6f3f0349b5f6a2957e7363f
Salvador Mercado <msalvadormg@gmail.com>
2021-09-27 13:23:24 -0500

    Added curios dependencie
    Curios added to gradle.build and loaded correctly

--- src/main/java/com/github/salvadormg15/rubber_duck/RubberDuck.java
+++ src/main/java/com/github/salvadormg15/rubber_duck/RubberDuck.java
@@ -2,8 +2,10 @@ package com.github.salvadormg15.rubber_duck;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,6 +21,7 @@ public class RubberDuck
     public RubberDuck() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
     	bus.addListener(this::setup);
+    	bus.addListener(this::curiosSetup);
     	Registries.BLOCKS.register(bus);
     	Registries.ITEMS.register(bus);
     	Registries.SOUND_EVENTS.register(bus);
@@ -29,6 +32,14 @@ public class RubberDuck
 
     private void setup(final FMLCommonSetupEvent event)
     {
+    	
+    }
+    private void curiosSetup(InterModEnqueueEvent event){
+    	if(!ModList.get().isLoaded("curios")){
+    		LOGGER.debug("Cannot find Curios in modloading");
+    		return;
+    	}
+    	
     }
 }
 
